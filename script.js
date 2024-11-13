e67700ed50d1e6ae6621816db743c41e78e11cce
kamalabhtt <kamalabhatt3213@gmail.com>
2024-11-13 21:31:18 +0545

    main

---
+++ script.js
@@ -0,0 +1,90 @@
+let btn=document.querySelector("#btn");
+let content=document.querySelector("#content");
+let voice=document.querySelector("#voice")
+
+function speak(text){
+    let text_speak=new SpeechSynthesisUtterance(text)
+    text_speak.rate=1
+    text_speak.pitch=1
+    text_speak.volume=1
+    text_speak.lang="ne-IN"
+    window.speechSynthesis.speak(text_speak)
+
+}
+
+function wishMe(){
+    let day=new Date()
+    let hours=day.getHours()
+    if(hours>=0 && hours<12){
+        speak("Good Morning Maam")
+    }
+
+    else if(hours>=12 && hours <16){
+        speak("Good afternoon maam")
+    }else{
+        speak("Good Evenning maam")
+    }
+
+}
+window.addEventListener('load',()=>{
+    wishMe()
+})
+let speechRecognition= window.speechRecognition || window.webkitSpeechRecognition
+let recognition = new speechRecognition()
+recognition.onresult=(event)=>{
+   let currentIndex= event.resultIndex
+   let transcript=event.results[currentIndex][0].transcript
+   content.innerText=transcript
+    takeCommand(transcript.toLowerCase())
+
+}
+btn.addEventListener("click",()=>{
+    recognition.start()
+    btn.style.display="none"
+    voice.style.display="block"
+})
+function takeCommand(message){
+    btn.style.display="flex"
+    voice.style.display="none"
+
+    if(message.includes("hello")||message.includes("hey")){
+        speak("hello maam, what can i help you?")
+
+    }
+    else if(message.includes("who are you?")){
+        speak("i am virtual assistant , created by kamala maam")
+
+    }else if(message.includes("open youtube")){
+        speak("opening youtube")
+        window.open("https://www.youtube.com/","_blank")
+
+    }else if(message.includes("open google")){
+        speak("opening google")
+        window.open("https://google.com/","_blank")
+
+    }else if(message.includes("open facebook")){
+        speak("opening facebook")
+        window.open("https://facebook.com/","_blank")
+        
+
+    }else if(message.includes("open calculator")){
+        speak("opening calculator")
+        window.open("calculator://")
+        
+    }else if(message.includes("time")){
+        let time=new Date().toLocaleString(undefined,{hour:"numeric",minute:"numeric"})
+        
+        
+    }else if(message.includes("date")){
+        let date=new Date().toLocaleString(undefined,{day:"numeric",month:"short"})
+    }// date ra time show garena 
+        
+        
+
+    else{
+        let finalText = "this is what i found on internet regarding" + message.replace("pinky","") || message.replace("pinky","");
+        speak(finalText);
+        window.open(`https://www.google.com/search?q=${message.replace("pinky","")}`, "_blank");
+    }
+}
+
